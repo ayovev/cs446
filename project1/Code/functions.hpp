17861da4ff4ceea8f084e32a3cddad5c899585b6
Alex Yovev <ayovev511@gmail.com>
2017-09-16 16:25:01 -0700

    added function prototype file to declutter main

---
+++ project1/Code/functions.hpp
@@ -0,0 +1,22 @@
+#include<cstdlib>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<map>
+#include<vector>
+
+using namespace std;
+
+void getMetadataFilepath(ifstream& fin, string& mdfp);
+void getComponentCycleTimes(ifstream& fin, map<string, int>& cycleTimes);
+void getLogFilepath(ifstream& fin, string& lfp);
+void readConfigurationFile(ifstream& fin, map<string, int>& cycleTimes, 
+                           string& mdfp, string& lfp);
+void readOneMeta(ifstream& fin, vector<string>& mdd, vector<char>& mdc, 
+                 vector<int>& cycles);
+void readMetadataFile(ifstream& fin, vector<string>& mdd, vector<char>& mdc, 
+                      vector<int>& cycles, int& count);
+int calculateTime(map<string, int>& cycleTimes, vector<string>& mdd, 
+                  vector<int>& mdc, int index);
+void logToMonitor(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco, 
+                  vector<int>& mdcy, string logFilepath, int logType, int count);
