3cc255fb93b32d383d5d2c54f712095800c0c5e9
Alex Yovev <ayovev511@gmail.com>
2017-09-19 18:21:52 -0700

    file / directory management

---
+++ project1/Code/Sim01_YovevAlex/functions.hpp
@@ -0,0 +1,32 @@
+#include<cstdlib>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<map>
+#include<vector>
+
+using namespace std;
+
+// function prototypes
+void getMetadataFilepath(ifstream& fin, string& mdfp);
+void getComponentCycleTimes(ifstream& fin, map<string, int>& cycleTimes);
+void getLogTypeAndFilepath(ifstream& fin, string& lfp, int& lt);
+void getLogType(ifstream& fin, int& lt);
+void getLogFilepath(ifstream& fin, string& lfp);
+void readConfigurationFile(ifstream& fin, map<string, int>& cycleTimes, 
+                           string& mdfp, string& lfp, int& lt);
+void readOneMeta(ifstream& fin, vector<string>& mdd, vector<char>& mdc, 
+                 vector<int>& cycles);
+void readMetadataFile(ifstream& fin, vector<string>& mdd, vector<char>& mdc, 
+                      vector<int>& cycles, int& count);
+int calculateTime(map<string, int>& cycleTimes, vector<string>& mdd, 
+                  vector<int>& mdc, int index);
+void logToMonitor(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco, 
+                  vector<int>& mdcy, string logFilepath, int logType, int count);
+void logToFile(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
+               vector<int>& mdcy, string logFilepath, int logType, int count);
+void log(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
+         vector<int>& mdcy, string logFilepath, int logType, int count);
+void checkConfigurationFile(ifstream& fin, const char *argv[]);
+void checkMetadataFile(ifstream& fin, string mdfp);
+int handleErrors(int e);
