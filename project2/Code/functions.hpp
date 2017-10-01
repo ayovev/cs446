36be4c7596951540fac08ebebee171d73bc51866
Alex Yovev <ayovev511@gmail.com>
2017-10-01 16:26:29 -0700

    reorganized function prototypes by alphabetical order

--- project2/Code/functions.hpp
+++ project2/Code/functions.hpp
@@ -8,27 +8,33 @@
 using namespace std;
 
 // function prototypes
-void getMetadataFilepath(ifstream& fin, string& mdfp);
+int calculateCycleTime(map<string, int>& cycleTimes, vector<string>& mdd, 
+                       vector<int>& mdc, const int index);
+double calculateSleepTime(map<string, int>& cycleTimes, vector<string>& mdd, 
+                          vector<int>& mdc, const int index);
+void checkConfigurationFile(ifstream& fin, const char *argv[]);
+void checkMetadataFile(ifstream& fin, const string mdfp);
 void getComponentCycleTimes(ifstream& fin, map<string, int>& cycleTimes);
-void getSystemMemory(ifstream& fin, int& sm, string& units);
 void getLogType(ifstream& fin, int& lt);
 void getLogFilepath(ifstream& fin, string& lfp);
 void getLogTypeAndFilepath(ifstream& fin, string& lfp, int& lt);
+void getMetadataFilepath(ifstream& fin, string& mdfp);
+void getSystemMemory(ifstream& fin, int& sm, string& units);
+int handleErrors(const int e, map<string, int>& cycleTimes, vector<string>& mdd,
+                 vector<char>& mdco, vector<int>& mdcy, const string logFilepath,
+                 const int logType, const int count, const int sm);
+void log(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
+         vector<int>& mdcy, const string logFilepath, const int logType, 
+         const int count, const int sm);
+void logToFile(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
+               vector<int>& mdcy, const string logFilepath, const int logType, 
+               const int count, const int sm);
+void logToMonitor(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
+                  vector<int>& mdcy, const string logFilepath, const int logType, 
+                  const int count, const int sm);
 void readConfigurationFile(ifstream& fin, map<string, int>& cycleTimes, 
                            string& mdfp, string& lfp, int& lt, int& sm, string& units);
 void readOneMeta(ifstream& fin, vector<string>& mdd, vector<char>& mdc, 
                  vector<int>& cycles);
 void readMetadataFile(ifstream& fin, vector<string>& mdd, vector<char>& mdc, 
                       vector<int>& cycles, int& count);
-int calculateCycleTime(map<string, int>& cycleTimes, vector<string>& mdd, 
-                  vector<int>& mdc, int index);
-void logToMonitor(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco, 
-                  vector<int>& mdcy, string logFilepath, int logType, int count);
-void logToFile(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
-               vector<int>& mdcy, string logFilepath, int logType, int count);
-void log(map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
-         vector<int>& mdcy, string logFilepath, int logType, int count);
-void checkConfigurationFile(ifstream& fin, const char *argv[]);
-void checkMetadataFile(ifstream& fin, string mdfp);
-int handleErrors(int e, map<string, int>& cycleTimes, vector<string>& mdd, vector<char>& mdco,
-                 vector<int>& mdcy, string logFilepath, int logType, int count);
